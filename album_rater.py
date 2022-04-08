798058287f128ce1efc7ae2012cd33e8209488b0
Kimberli Jones <103225527+kimberli-skye@users.noreply.github.com>
2022-04-08 12:29:40 +1200

    Plan Comments
    A range of comments detailing each function needed to complete

---
+++ album_rater.py
@@ -0,0 +1,38 @@
+#
+# album_rater.py
+#
+# Allows user to add, delete, and rate albums
+# Recommends albums based off of genre
+#
+# KJ
+# 08/04 to
+
+
+# Functions
+
+# Function that manages the album dictionary
+
+
+# Prints album dictionary (basic)
+
+
+# Prints album dictionary (advanced)
+
+
+# Adds album to dictionary
+
+
+# Edit album in dictionary
+
+
+# Delete album from dictionary
+
+
+# Rate album
+
+
+# Reccomends albums with the same genre
+
+
+
+# Main routine
