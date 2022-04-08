a1c0d9c6b723c6ea35c7cb17eeb0c518cce61f24
Kimberli Jones <103225527+kimberli-skye@users.noreply.github.com>
2022-04-08 12:45:09 +1200

    Created album dictionary, Created basic print dict
    - Added the album dictionary function, filled it with three albums of my choice
    - Created the print (basic) function, prints each value of the album in a tidy format using a for loop
    - Accessed these through the main routine
    - Considered not having an advanced printing dictionary function

--- album_rater.py
+++ album_rater.py
@@ -11,9 +11,26 @@
 # Functions
 
 # Function that manages the album dictionary
+def album_dictionary():
+    """ Dictionary of chosen albums (not user entered).
+        Returns the dictionary (with a mix of integers and strings).
+    """
+    albums = {1:{"Title":"Be More Chill", "Artist":"Original Cast of Be More Chill", "Genre":"Musical", "Rating":""},
+                      2:{"Title":"Electra Heart", "Artist":"MARINA", "Genre":"Pop", "Rating":""},
+                      3:{"Title":"Eye of the Storm", "Artist":"Watt White", "Genre":"Rock", "Rating":""},
+        }
+
+    return albums
 
 
 # Prints album dictionary (basic)
+def print_dictionary_basic(dictionary):
+    """ Accepts a Dictionary and loops through it.
+        Prints the keys and values (Album and its differnet values).
+    """
+    for ID, album in dictionary.items():
+        print("Album {}, Album: {}: \nArtist: {} \nGenre: {} \nRating: {}\n".format(ID, album["Title"], album["Artist"],
+                                                                                                                        album["Genre"], album["Rating"]))
 
 
 # Prints album dictionary (advanced)
@@ -36,3 +53,9 @@
 
 
 # Main routine
+if __name__ == "__main__":
+    # Accesses the album dictionary
+    album_dict = album_dictionary()
+
+    # Uses basic dictionary print to print the albums
+    print_dictionary_basic(album_dict)
