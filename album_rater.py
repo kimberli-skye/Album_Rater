768d8821655282ee76ec88d9682869d3d9cfe719
Kimberli Jones <103225527+kimberli-skye@users.noreply.github.com>
2022-05-02 14:19:15 +1200

    Edit album function, fixed bugs, doc strings added

--- album_rater.py
+++ album_rater.py
@@ -7,11 +7,12 @@
 # KJ
 # 08/04 to
 
-
 # Functions
-
-# Menu that allows the user to access each function when needed
 def menu():
+    """ Prints menu that user can choose function from.
+         Checks input is valid.
+         Returns mode.
+    """
     while True:
         try:
             mode = int(input("""\nChoose a mode:
@@ -31,78 +32,78 @@ def menu():
             if mode in [0, 1, 2, 3, 4, 5]:
                 return mode
                 break
-            
             else:
                 print("Not valid number, enter another.")
 
-# Function that manages the album dictionary
+
 def album_dictionary():
     """ Dictionary of chosen albums (not user entered).
-        Returns the dictionary (with a mix of integers and strings).
+         Returns the dictionary (with a mix of integers and strings).
     """
-    albums = {1:{"Title":"Be More Chill", "Artist":"Original Cast of Be More Chill", "Genre":"Musical", "Rating":""},
-                      2:{"Title":"Electra Heart", "Artist":"MARINA", "Genre":"Pop", "Rating":""},
-                      3:{"Title":"Eye of the Storm", "Artist":"Watt White", "Genre":"Rock", "Rating":""},
-        }
-
+    albums = {}
     return albums
 
 
-# Prints album dictionary (basic)
 def print_dictionary_basic(dictionary):
     """ Accepts a Dictionary and loops through it.
-        Prints the keys and values (Album and its differnet values).
+         Prints the keys and values (Album and its differnet values).
     """
     for ID, album in dictionary.items():
         print("Album: {} \nArtist: {} \nGenre: {} \nRating: {}\n".format(album["Title"], album["Artist"],
                                                                                                                 album["Genre"], album["Rating"]))
 
 
-# Adds album to dictionary
+def choose_album(album_dict):
+    """ Prints the number needed to choose an album.
+         Checks if the ID number is valid and returns it.
+    """
+    for ID, album in album_dict.items():
+        print("Enter {} to choose the album {}".format(ID, album["Title"]))
+
+    chosen_id = int(input(""))
+    for ID in album_dict:
+        if chosen_id == ID:
+            return chosen_id
+
+
 def add_album():
-    # Add album title
-    title = input("What is the title of the album? ")
-    # Add album Artist
+    """ Gets user input for each value.
+         Returns the different strings and count.
+    """
+    title = input("What is the title of the album?: ")
     artist = input("What is the artist of the album? ")
-    # Add album Genre
     genre = input("What is the genre of the album? ")
 
     return title, artist, genre
 
 
-# Allows user to choose an album
-# Prints the number needed to choose an album
-# Checks if the ID number is valid
-# Returns the integer if it is
-def choose_album(album_dict):
-    for ID, album in album_dict.items():
-        print("Enter {} to choose the album {}".format(ID, album["Title"]))
+def edit_album(album_dictionary, chosen_album):
+    """ Calls the add_album function.
+         replaces the original album with edited album.
+    """
 
-    chosen_ID = int(input(""))
-
-    for ID in album_dict:
-        if chosen_ID == ID:
-            return chosen_ID
-        
+    print("Editing album:", album_dictionary[chosen_album])
+    title, artist, genre = add_album()
+    album_dict[chosen_album] = {"Title":title, "Artist":artist, "Genre":genre, "Rating":""}
 
 
-# Edit album in dictionary
+def delete_album(album_dictionary, chosen_album):
+    """ Takes the user chosen album.
+         Deletes it from the dictionary.
+    """
+    del album_dictionary[chosen_album]
 
 
-# Delete album from dictionary
-
-
+    
 # Rate album
-
-
 # Reccomends albums with the same genre
-
-
-
 # Main routine
 if __name__ == "__main__":
     # Accesses the album dictionary
     album_dict = album_dictionary()
+
+    # Defines constants
+    count = 0
 
     # Sets the prgram running top True
     running =  True
@@ -115,13 +116,19 @@ if __name__ == "__main__":
             # Adds an album using add album function
             # Appends the string input to dictionary
             title, artist, genre = add_album()
-            album_dict[4] = {"Title":title, "Artist":artist, "Genre":genre, "Rating":""}
+            album_dict[count] = {"Title":title, "Artist":artist, "Genre":genre, "Rating":""}
+            count = count + 1
 
         elif chosen_mode == 2:
-            print(choose_album(album_dict))
+            # Calls chosen album
+            chosen_album = choose_album(album_dict)
+            edit_album(album_dict, chosen_album)
 
         elif chosen_mode == 3:
-            skip
+            # Calls chosen_album function
+            # Calls delete_album function
+            chosen_album = choose_album(album_dict)
+            delete_album(album_dict, chosen_album)
 
         elif chosen_mode == 4:
             skip
